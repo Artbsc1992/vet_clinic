17d1abfcb1b94efaa328758695b61f18345eabde
arturo <ahpareja@gmail.com>
2022-07-05 14:14:28 -0500

    Add new values

--- data.sql
+++ data.sql
@@ -6,3 +6,13 @@ VALUES
 ('Gabumon', 'Nov 15, 2018', 2, true, 8.0), 
 ('Pikachu', 'Jan 7, 2021', 1, false, 15.04), 
 ('Devimon', 'May 12, 2017', 5, true, 11.0);
+
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) 
+VALUES 
+('Charmander' , 'Feb 8, 2020' , 0, false, -11.0), 
+('Plantmon' , 'Nov 15, 2021' , 2, true, -5.70), 
+('Squirtle' , 'Apr 2, 1993' , 3, false, -12.13), 
+('Angemon' , 'Jun 12, 2005' , 1, true, -45.0), 
+('Boarmon' , 'Jun 7, 2005' , 7, true, 20.40), 
+('Blossom' , 'Oct 13, 1998' , 3, true, 17.0), 
+('Ditto' , 'May 14, 2022' , 4, true, 22.0);
